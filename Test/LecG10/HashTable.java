e4ccbb296ac91b021674af2e2f6d092d23ebbda4
Sudhakar Kumar <91364666+savagePrince04@users.noreply.github.com>
2024-10-13 11:35:24 +0530

    Delete Test/LecG10 directory

--- Test/LecG10/HashTable.java
+++
@@ -1,9 +0,0 @@
-package Test.LecG10;
-import java.util.Hashtable;
-public class HashTable {
-    public static void main(String[] args) {
-        Hashtable<Integer,String> sc=new Hashtable<>();
-        sc.put(106,"Sudhakr");
-        sc.put(101,"Tushar");
-    }
-}
